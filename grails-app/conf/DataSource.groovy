4b722ddc7bd6a0bb9eae5e143448569eb1d99f90
Stuart Watt <stuart@morungos.com>
2012-01-25 16:04:13 -0500

    resolved conflict

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,6 +1,8 @@
 //check external configuration as described in Config.groovy
-
 datasource {
 	grails.mongo.host = "localhost"
 	grails.mongo.databaseName = "capsid"
+	grails.mongo.port = 27017
+	grails.gridfs = grails.mongo.databaseName
 }
+
