9cd6e64af48de963f10a4553590d774199f25036
Philippe Laflamme <philippe.laflamme@gmail.com>
2012-01-24 21:18:10 -0500

    Fixing WAR file to exclude Jetty and servlet API jar files

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,7 +1,9 @@
-unJarTask = {jarName,includePattern ->
+import org.codehaus.groovy.grails.resolve.IvyDependencyManager;
+
+unJarTask = {jarFile,includePattern ->
 
     ant.mkdir(dir:"$stagingDir/tmp/")
-    ant.unjar(src:"$stagingDir/WEB-INF/lib/$jarName", dest:"$stagingDir/tmp")
+    ant.unjar(src:jarFile, dest:"$stagingDir/tmp")
     ant.move(todir:"$stagingDir"){
         fileset(dir:"$stagingDir/tmp"){
             include (name: "$includePattern/**")
@@ -13,9 +15,18 @@ unJarTask = {jarName,includePattern ->
 eventCreateWarStart = {warName, stagingDir ->
     ant.mkdir(dir:"$stagingDir/ca/on/oicr/ferv/")
     ant.move(file:"$stagingDir/WEB-INF/classes/ca/on/oicr/ferv/Start.class",todir:"$stagingDir/ca/on/oicr/ferv/")
-    unJarTask("jetty-6.1.14.jar","org")
-    unJarTask("jetty-util-6.1.14.jar","org")
-    unJarTask("servlet-api-2.5-6.1.14.jar","javax")
+
+    IvyDependencyManager idm = grailsSettings.dependencyManager
+    report = idm.resolveDependencies(IvyDependencyManager.PROVIDED_CONFIGURATION)
+    for(a in report.allArtifactsReports) {
+      if(a.artifact.moduleRevisionId.organisation == "org.mortbay.jetty") {
+        unJarTask(a.localFile,"*")
+      }
+    }
+
+//    unJarTask("jetty-6.1.14.jar","org")
+//    unJarTask("jetty-util-6.1.14.jar","org")
+//    unJarTask("servlet-api-2.5-6.1.14.jar","javax")
 }
 
 
@@ -27,6 +38,5 @@ eventCreateWarEnd = {warName, stagingDir ->
     }
     ant.jar(destfile:warName, update:true) {
         manifest { attribute(name: "Main-Class", value: "ca.on.oicr.ferv.Start")}
-        manifest { attribute(name: "Class-Path", value: "$libPath") }
     }
 }
