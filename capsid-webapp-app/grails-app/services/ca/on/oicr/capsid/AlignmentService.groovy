2392366ea124be4c548e963a391b8499b5420d12
Stuart Watt <stuart@morungos.com>
2014-01-27 17:16:04 -0500

    Added merging logic for the genome relative abundance. This is, as you may surmise, a lot of code.

--- capsid-webapp-app/grails-app/services/ca/on/oicr/capsid/AlignmentService.groovy
+++ capsid-webapp-app/grails-app/services/ca/on/oicr/capsid/AlignmentService.groovy
@@ -12,6 +12,7 @@ package ca.on.oicr.capsid
 
 import org.bson.types.ObjectId
 import com.mongodb.BasicDBList
+import com.mongodb.BasicDBObject
 import grails.plugins.springsecurity.Secured
 
 /**
@@ -104,7 +105,7 @@ class AlignmentService {
     }
 
 
-    Map buildRelativeAbundanceTree(alignment) {
+    List buildRelativeAbundanceTree(alignment) {
 
         // Loosely, this is actually a kind of garbage collection process. Stuff that is too small
         // we mark, and then we can sweep through the tree. 
@@ -117,6 +118,7 @@ class AlignmentService {
         Map entryTable = [:] as Map
         Map levelTable = [:] as Map
         Map markTable = [:] as Map
+        Map scoreTable = [:] as Map
 
         List levels = ["SPECIES", "GENUS", "FAMILY", "ORDER"]
         Map parentLevelMap = [
@@ -130,9 +132,14 @@ class AlignmentService {
         for(element in alignment.gra) {
             String level = element["level"]
             String parentLevel = parentLevelMap[level]
-            String parent = element["parent"] ?: "oi:" + parentLevel  
+            String parent = element["parent"] ?: "oi:" + parentLevel
+
+            if (level == "SEQUENCE") continue
+
+            //log.info("Basic element: " + element["id"] + ", " + parent + ", " + level)
 
             entryTable[element["id"]] = element
+            scoreTable[element["id"]] = element["score"]
 
             if (childTable[parent] == null) childTable[parent] = [] as List
             childTable[parent] << element["id"]
@@ -150,9 +157,15 @@ class AlignmentService {
             if (nextLevel == null) continue
             String nextParent = "oi:" + nextLevel
 
+            //log.info("Adding level: " + key + ", " + nextParent)
+
             parentTable[key] = nextParent
             if (childTable[nextParent] == null) childTable[nextParent] = [] as List
+            childTable[nextParent] << key
+            entryTable[key] = [level: level]
         }
+
+        //log.info("Root children: " + childTable["oi:ROOT"])
 
         // Step 2A. Work down through the tree. Well, probably not actual recursion as we are working
         // a level at a time. At each level, mark the ones we want to keep.
@@ -163,7 +176,7 @@ class AlignmentService {
             // are done. At each level, keep the entries that match our criteria and mark all the rest
             // for collection. 
 
-            Integer maxCount = 5
+            Integer maxCount = 10
             Integer count = 0
             for(elementId in elementIds) {
                 markTable[elementId] = (count++ >= maxCount)
@@ -174,12 +187,14 @@ class AlignmentService {
         // element. 
         def handleMarking = { id, marked ->
             if (marked) markTable[id] = marked
+            if (marked) log.info("Marking as mergable: " + id)
             for(child in childTable[id]) {
-                handleMarking(child, markTable[id])
+                call(child, markTable[id])
             }
         }
 
         for(child in childTable["ROOT"]) {
+            //log.info("Handling marking: " + child)
             handleMarking(child, markTable[child])
         }
 
@@ -190,16 +205,27 @@ class AlignmentService {
         // Combines two entities, and all their descendants, which must be marked, into a new 
         // block, which is then returned. 
         def mergeEntities = { id1, id2 ->
-            assert childTable[id1].length <= 1
-            assert childTable[id2].length <= 1
-            assert childTable[id1] == childTable[id2]
+            //log.info("mergeEntities: " + id1 + ", " + id2)
+            assert id1 != null
+            assert id2 != null
+
+            // When we merge entities, we should to check the number of mergable children
+            // is zero or one. 
+
+            def children1 = (childTable[id1] ?: []).findAll { markTable[it] }
+            def children2 = (childTable[id2] ?: []).findAll { markTable[it] }
+
+            assert children1.size() <= 1
+            assert children2.size() <= 1
 
             def child = null
-            if (childTable[id1].length) {
-                child = mergeEntities(childTable[id1][0], childTable[id1][1])
+            if (children1.size() > 0 && children2.size()) {
+                //log.info("Merging children: " + children1 + ", " + children2)
+                child = call(children1[0], children2[0])
             }
 
             entryTable[id1]["score"] += entryTable[id2]["score"]
+            scoreTable[id1] += scoreTable[id2]
             childTable[parentTable[id2]].remove(id2)
             parentTable.remove(id2)
             childTable.remove(id2)
@@ -208,53 +234,153 @@ class AlignmentService {
                 childTable[id1] = [child]
                 parentTable[child] = id1
             }
+
+            return id1
         }
 
         def handleMerging = { id ->
             List children = childTable[id]
+            //log.info("handleMerging: " + id + ", " + children)
             if (children == null) return
-            if (children.length == 0) return
+            if (children.size() == 0) return
 
+            // First of all, merge all child nodes if we can
             for(child in children) {
-                handleMerging(child)
+                call(child)
             }
 
+            // This list will contain all the marked elements, and we can merge these
             List mergable = children.findAll { markTable[it] }
-            if (mergable.length > 0) {
+            if (mergable.size() > 0) {
+                //log.info("About to merge: " + id + " => " + mergable)
                 def initial = mergable.pop()
-                mergable.inject(initial) { acc, val -> mergeEntities(acc, val) }
+                mergable.inject(initial) { acc, val -> mergeEntities.call(acc, val) }
             }
 
             Boolean entirelyMergable = ! children.find { ! markTable[it] }
+            //log.info("Anything else? " + id + " => " + entirelyMergable + ", " + children.find { ! markTable[it] })
             if (entirelyMergable) {
+                //log.info("Marking as mergable: " + id)
                 markTable[id] = true
             }
         }
 
-        handleMerging("ROOT")
+        handleMerging("oi:ROOT")
 
         // Step 3. We need to recalculate the actual breakdowns from the root down. This
         // assumes that at each level we have a total of 1.0, which gets evenly distributed
-        // across everything. 
+        // across everything. We can also remove any items which have a zero score after 
+        // we have done this. The result is basically the hierarchy we want to send back
+        // to the server. Also, anything that is marked can be tagged as a placeholder, as 
+        // these are residuals. 
 
-        def displayTree = { id, depth ->
-            StringBuilder sb = new StringBuilder()
-            sb.append("         ".substring(0, depth))
-            sb.append(id)
-            if (markTable[id]) sb.append("*")
-            sb.append(" ")
-            sb.append(entryTable[id]?.getAt("score"))
-            System.err.println(sb.toString())
-            
-            List children = childTable[id]
-            if (children == null) return
-            for(child in children) {
-                displayTree(child, depth + 2)
+        def removedMarkedSubtrees = { id ->
+            Boolean parentMarked = markTable[id]
+            for(child in (childTable[id] ?: [])) {
+                if (parentMarked && markTable[child]) {
+                    //log.info("Removing subtree: " + id)
+                    childTable[id] = []
+                    break
+                }
+                call(child)
             }
         }
 
-        System.err.println("")
-        displayTree("ROOT", 0)
+        removedMarkedSubtrees("oi:ROOT")
+
+        def calculateScores = { id ->
+            def children = childTable[id] ?: []
+            //log.info("Scoring: " + id + ", " + children)
+            if (children.size() != 0 && scoreTable[id] == null) {
+                Float total = 0.0
+                for(child in children) {
+                    //log.info("Scoring: " + id + " -> " + child)
+                    total = total + (call(child) ?: 0.0)
+                }
+                scoreTable[id] = total
+            }
+            return scoreTable[id]
+        }
+
+        def scaleScores = { id, factor -> 
+            if (scoreTable[id] != null) scoreTable[id] = scoreTable[id] * factor
+            if (childTable[id] != null) {
+                for(child in childTable[id]) {
+                    call(child, factor)
+                }
+            }
+        }
+
+        scaleScores("oi:ROOT", 1 / calculateScores("oi:ROOT"))
+
+        def sortScores = { id ->
+            if (childTable[id] != null) {
+                childTable[id] = childTable[id].grep { scoreTable[it] }.sort { a, b ->
+                    //log.info("Sorting: " + a + ", " + b + " -> " + markTable[b])
+                    if (markTable[b]) {
+                        return -1
+                    } else {
+                        return 0
+                    }
+                }
+                for(child in childTable[id]) {
+                    call(child)
+                }
+            }
+        }
+
+        sortScores("oi:ROOT")
+
+        // def displayTree = { id, depth ->
+        //     StringBuilder sb = new StringBuilder()
+        //     sb.append("         ".substring(0, depth))
+        //     sb.append(id)
+        //     if (markTable[id]) sb.append("*")
+        //     sb.append(" ")
+        //     sb.append(entryTable[id]?.getAt("score"))
+        //     sb.append(" -> ")
+        //     sb.append(scoreTable[id])
+        //     System.err.println(sb.toString())
+        //     
+        //     List children = childTable[id]
+        //     if (children == null) return
+        //     for(child in children) {
+        //         call(child, depth + 2)
+        //     }
+        // }
+
+        // We're doing a breadth first walk through the tree, so 
+        def generateResults = { id ->
+            BasicDBList results = [] as BasicDBList
+            List queue = [] as List
+            queue << id
+            while(queue.asBoolean()) {
+                String next = queue.remove(0)            // Remove from the head of queue
+                if (childTable[next] != null) queue.addAll(childTable[next])
+                results << [
+                    id: (markTable[next] ? "mi:" + next : next),
+                    level: entryTable[next]?.getAt("level"),
+                    score: scoreTable[next],
+                    parent: (markTable[parentTable[next]] ? "mi:" + parentTable[next] : parentTable[next]),
+                    placeholder: next.startsWith("oi:"),
+                    name: (markTable[next] ? "Other" : entryTable[next]?.getAt("name")) ?: ""
+                ]
+            }
+            
+            return results
+        }
+
+        // System.err.println("")
+        // displayTree("oi:ROOT", 0)
+
+        def results = generateResults("oi:ROOT")
+        results.remove(0)
+        //log.info(results)
+        return results
+
+        // Now we can generate the data we need, which is the normalized and merged 
+        // genome relative abundance. This is very similar to the recursive crawl, but 
+        // is actually simpler. 
 
 //        log.info("Parents: " + parentTable.toString())
 //        log.info("Children: " + childTable.toString())
@@ -277,43 +403,6 @@ class AlignmentService {
         // 10 species, we cannot have >10 genera, and so on, but what determines which
         // are accumulated is different. 
 
-        buildRelativeAbundanceTree(alignment)
-
-        List input = alignment.gra
-        Integer maxCount = 5
-        Integer count = 0
-        Boolean accumulating = false
-
-        BasicDBList result = [] as BasicDBList
-        Integer i = 0
-        String currentRank
-
-        for(element in input) {
-            String rank = element["level"]
-            if (rank == "SEQUENCE")
-                continue;
-
-            if (rank == currentRank) {
-                if (accumulating) {
-                    result.last()["score"] += element["score"]
-                } else if (count++ == maxCount) {
-                    result << element
-                    result.last()["name"] = "Other"
-                    result.last()["id"] = "oi:"
-                    accumulating = true
-                } else {
-                    result << element
-                }
-            } else {
-                count = 0
-                accumulating = false
-                currentRank = rank
-                result << element
-            }
-        }
-
-        log.info result.toString()
-
-        return result.toString()
+        return buildRelativeAbundanceTree(alignment)
     }
 }
